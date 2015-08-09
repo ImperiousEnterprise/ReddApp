db0e0395c5fa18f652604542ed332edbba5a1644
Adefemi Adeyemi <ImperiousEnterprise@hotmail.com>
2015-08-10 01:09:23 +0900

    Deploy

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  before_save :downcase_email
+  has_many :links
+  validates :name , presence: true , length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email , presence: true , length: { maximum: 255 } , format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
+end
